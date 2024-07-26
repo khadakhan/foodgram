df872401724205e44009b0fa71036fdf5ded803e
Konstantin Khadakhanov <k-project@mail.ru>
2024-07-27 00:13:29 +0300

    flake8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
+from django.urls import include, path
+from rest_framework import routers
 from recipes.views import (IngredientViewSet,
                            RecipeViewSet,
                            TagViewSet)
 from users.views import (CustomTokenCreateView,
                          CustomUserViewSet)
-from django.urls import include, path
-from rest_framework import routers
 
 app_name = 'api'
 
