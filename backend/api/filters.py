884f4da2d2327a8dfa0a566172e9ef86612207a3
Konstantin Khadakhanov <k-project@mail.ru>
2024-07-31 19:16:06 +0300

    review 1-1 Recipe filtration

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,47 +1,50 @@
 from django_filters import rest_framework as filters
 
-from recipes.models import Recipe, RecipeTag, Tag
+from recipes.models import (
+    Recipe,
+    Tag
+)
+
+TAGS = [(tag.slug, tag.id) for tag in Tag.objects.all()]
+IS_IN = [(0, 'не добавлен'), (1, 'добавлен')]
 
 
 class RecipeFilter(filters.FilterSet):
+    author = filters.CharFilter(
+        field_name='author',
+    )
+    tags = filters.MultipleChoiceFilter(
+        field_name='tags__slug',
+        distinct=True,
+        choices=TAGS
+    )
+    is_in_shopping_cart = filters.ChoiceFilter(
+        choices=IS_IN,
+        method='filter_is_in'
+    )
+    is_in_favorite = filters.ChoiceFilter(
+        choices=IS_IN,
+        method='filter_is_in'
+    )
+
+    def filter_is_in(self, queryset, name, value):
+        user = self.request.user
+        if user.is_authenticated and value == '1':
+            if name == 'is_in_shopping_cart':
+                in_list = user.what_by_user.all().values_list(
+                    'recipe',
+                    flat=True
+                )
+            if name == 'is_in_favorite':
+                in_list = user.recipes_in_favorites.all().values_list(
+                    'recipe',
+                    flat=True
+                )
+            queryset = queryset.filter(id__in=in_list)
+        return queryset
+
     class Meta:
         model = Recipe
         fields = (
             'author',
         )
-
-    @property
-    def qs(self):
-        queryset = super().qs
-        user = self.request.user
-        author_id = self.request.query_params.get('author')
-        is_favorited = self.request.query_params.get('is_favorited')
-        is_in_shopping_cart = self.request.query_params.get(
-            'is_in_shopping_cart'
-        )
-        tag_slugs = self.request.query_params.getlist('tags')
-        if author_id is not None and author_id.isdigit():
-            queryset = queryset.filter(author=author_id)
-        if tag_slugs:
-            tags = Tag.objects.filter(slug__in=tag_slugs).all()
-            tag_recipe_queryset_id = RecipeTag.objects.filter(
-                tag__in=tags
-            ).values_list(
-                'recipe',
-                flat=True
-            )
-            queryset = queryset.filter(id__in=tag_recipe_queryset_id)
-        if user.is_authenticated:
-            if is_favorited == '1':
-                in_favorite = user.recipes_in_favorites.all().values_list(
-                    'recipe',
-                    flat=True
-                )
-                queryset = queryset.filter(id__in=in_favorite)
-            if is_in_shopping_cart == '1':
-                in_shopping_cart = user.what_by_user.all().values_list(
-                    'recipe',
-                    flat=True
-                )
-                queryset = queryset.filter(id__in=in_shopping_cart)
-        return queryset
