cf74c991d17819c2c4d41f9710a7a5549f2ca29a
Konstantin Khadakhanov <k-project@mail.ru>
2024-08-12 02:46:55 +0300

    review 3-3

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
 from django.http import FileResponse
-from django.db.models import Count, Exists, OuterRef, Sum
+from django.db.models import Exists, OuterRef, Sum
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
