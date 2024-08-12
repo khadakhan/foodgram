32eb07ee5d5fbfec124e57ed0903528f70a53b65
Konstantin Khadakhanov <k-project@mail.ru>
2024-08-12 03:01:22 +0300

    review 3-4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.utils.datastructures import MultiValueDictKeyError
 from djoser.serializers import UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
