1c2d0494fb1c6085bd33449fa45e2e829fadb832
Konstantin Khadakhanov <k-project@mail.ru>
2024-08-13 23:39:10 +0300

    review 6-4

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,8 +1,9 @@
 import short_url
 from django.shortcuts import redirect
+from recipes.const import DOMAIN
 
 
 def redirect_view(request, surl):
     decoded_id = short_url.decode_url(surl)
-    url = f'https://foodgramdo.zapto.org/recipes/{decoded_id}/'
+    url = f'https://{DOMAIN}/recipes/{decoded_id}/'
     return redirect(url)
