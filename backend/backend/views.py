8974959f269e7ca3ac1eee88d68806ce0a463c00
Konstantin Khadakhanov <k-project@mail.ru>
2024-08-13 19:24:44 +0300

    review 6-3

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import redirect
 
 def redirect_view(request, surl):
     decoded_id = short_url.decode_url(surl)
-    url = f'/api/recipes/{decoded_id}/'
+    url = f'https://foodgramdo.zapto.org/recipes/{decoded_id}/'
     return redirect(url)
